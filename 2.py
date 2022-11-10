adbfd58701550ed4279c83a41f6d9d788747072f
shin8u <98867521+shin8u@users.noreply.github.com>
2022-11-11 00:55:50 +0300

    Update 2.py

--- 2.py
+++ 2.py
@@ -5,17 +5,28 @@ for i in range(0, n):
     x = int(input())
     a.append(x)
 a.sort()
-snowman.append(a[0])
-i = 1
+print(n)
+print(a)
 f = 0
-while i < n:
-    if a[i] != a[i-1]:
-        if len(snowman) < 3:
-            snowman.append(a[i])
-        if len(snowman) == 3:
-            print(snowman)
-            snowman.clear()
-            i -= 1
-            f += 1
-    i += 1
+l = n // 3
+for j in range(0, l):
+    i = 0
+    while i < n:
+        if len(snowman) == 0:
+            if a[i] != 0:
+                snowman.append(a[i])
+                a[i] = 0
+            i += 1
+        else:
+            if a[i] != snowman[len(snowman) - 1] and a[i] != 0:
+                if len(snowman) < 3:
+                    snowman.append(a[i])
+                    a[i] = 0
+                if len(snowman) == 3:
+                    snowman.sort()
+                    print(snowman)
+                    snowman.clear()
+                    i -= 1
+                    f += 1
+            i += 1
 print(f)
